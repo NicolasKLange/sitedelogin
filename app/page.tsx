e85d1b633cc6b445c5d6e5a5feab65bc0c7faae8
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2023-12-04 16:30:55 -0300

    1.0

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <div>
+    <table>
+    <tr>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+    </tr>
+    <tr>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+    </tr>
+    <tr>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+    </tr>
+</table>
+</div>
+    )
+}
