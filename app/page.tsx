a5b0bc8aa6ee5b9a86b99849b735b6b79033f8ea
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2023-12-06 16:05:40 -0300

    Jogo da Velha 2.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,48 @@
+import { MdExposurePlus1 } from "react-icons/md";
+
+
+
 export default function Home() {
-  return (
+  return (  
     <div>
-    <table>
-    <tr>
-        <td>x</td>
-        <td>x</td>
-        <td>x</td>
-    </tr>
-    <tr>
-        <td>x</td>
-        <td>x</td>
-        <td>x</td>
-    </tr>
-    <tr>
-        <td>x</td>
-        <td>x</td>
-        <td>x</td>
-    </tr>
-</table>
-</div>
-    )
+      <h1> - Jogo da Velha - </h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>X</td>
+            <td>X</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>X</td>
+            <td>X</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>X</td>
+            <td>X</td>
+            <td>X</td>
+          </tr>
+        </tbody>
+      </table>
+        <div className="Jogadores">
+            <div className="jogador1"> 
+                <h2>Jogador 1</h2>
+                    <div className="botaoJogador1"> 
+                        <button>ola</button>
+                    </div>
+            </div>
+                <button>
+                    <a href="/"><MdExposurePlus1 /></a>
+                </button>
+            <div className="jogador2"> 
+                <h2>Jogador 2</h2>
+                    <div className="botaoJogador2"> 
+                        <button>ola</button>
+                    </div>
+            </div>
+        </div>
+    </div>
+    
+  )
 }
