5cac0af81f2854e845e63b4e648500b5036bf03d
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2023-12-07 15:26:52 -0300

    Jogo da velha 2.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-export default function Page() {
-    return(
-      <div>
-        <div className="titulo">
-        <h1>Jogo da Velha</h1>
-            <a href="./jogo" >OLA</a>        
+import { MdExposurePlus1 } from "react-icons/md";
+
+
+
+export default function page() {
+  return (  
+    <div>
+      <h1> - Jogo da Velha - </h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>X</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+            <tr>
+              <td>X</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+            <tr>
+              <td>X</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="jogadores">
+            <h2>Jogador 1</h2>
+                <button><a href="/"><MdExposurePlus1 /></a></button>
+            <h2>Jogador 2</h2>
         </div>
-      </div>
+    </div>
     )
-  }
+}
