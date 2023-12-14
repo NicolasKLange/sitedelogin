8a15f8e884a1fbe60698cdaf090b6117bca1a710
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2023-12-14 15:44:13 -0300

    Adicionar pontos automático

--- app/jogo/page.tsx
+++ app/jogo/page.tsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 import { BsArrowCounterclockwise } from "react-icons/bs";
 import { FaHome } from "react-icons/fa";
 import { BsShare } from "react-icons/bs";
-import { IoIosAddCircleOutline } from "react-icons/io";
-
 
 
 export default function Home() {
+  const [table, setTable] = useState(["", "", "", "", "", "", "", "", ""])
+  const [player, setPlayer] = useState("X")
+
   function copyText() {
     // Pega o texto que deseja copiar
     const copyText = "https://jogo-da-velha-next-peach.vercel.app/";
@@ -19,114 +20,180 @@ export default function Home() {
     alert("Link copiado para a área de transferência");
   }
 
-
-  let pontosX = 0
-  let pontosO = 0
-
-
-  function addPointsX() {
-    pontosX = pontosX + 1
-    let count = document.querySelector("IoIosAddCircleOutline")
-    if (count) count.innerHTML = pontosX.toString()
-    console.log(pontosX)
-  }
-
-  function addPointsO() {
-    pontosO = pontosO + 1
-    let count = document.querySelector("IoIosAddCircleOutline")
-    if (count) count.innerHTML = pontosO.toString()
-    console.log(pontosO)
-  }
-
-  const [table, setTable] = useState(["", "", "", "", "", "", "", "", ""])
-
-  const [player, setPlayer] = useState("X")
-
-  const playerX = 0
-  const playerO = 0
-
   function mark(index: number) {
     if (table[index]) return
     table[index] = player
     setPlayer(player == "X" ? "O" : "X")
-    checkWinner()
+    checkWinner()   
   }
+
+  const [x, setx] = useState(0)
+  const [o, seto] = useState(0)
 
   function checkWinner() {
     if (table[0] == table[1] && table[1] == table[2] && table[0] != "") {
       if (player == "X") {
-        document.body.style.background = "green";
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
     }
-
+    
     if (table[3] == table[4] && table[4] == table[5] && table[3] != "") {
       if (player == "X") {
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
+      
     }
 
     if (table[6] == table[7] && table[7] == table[8] && table[6] != "") {
       if (player == "X") {
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
+      
     }
 
     if (table[0] == table[3] && table[3] == table[6] && table[0] != "") {
       if (player == "X") {
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
+      
     }
 
     if (table[1] == table[4] && table[4] == table[7] && table[1] != "") {
       if (player == "X") {
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
+      
     }
 
     if (table[2] == table[5] && table[5] == table[8] && table[2] != "") {
       if (player == "X") {
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
+      
     }
 
     if (table[0] == table[4] && table[4] == table[8] && table[0] != "") {
       if (player == "X") {
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
+      
     }
 
     if (table[2] == table[4] && table[4] == table[6] && table[2] != "") {
       if (player == "X") {
-        console.log("O X ganhou"); return
+        document.body.style.backgroundColor = "green"
+        alert("O X ganhou"); 
+        setx(x+1);
+        setTable (["", "", "", "", "", "", "", "", ""]);
+        setPlayer ("X")
+        return
       }
       else {
-        console.log("O O ganhou"); return
+        document.body.style.backgroundColor = "red"
+        alert("O O ganhou");
+        seto(o + 1);
+        setTable(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("X");
+        alert("O O ganhou"); return
       }
     }
+    if(table[0] != "" && table[1] != "" && table[2] != "" && table[3] != "" && table[4] != "" && table[5] != "" && table[6] != "" && table[7] != "" && table[8] != "" ){ 
+      setTable(["", "", "", "", "", "", "", "", ""]);
+      setPlayer("X"); 
+      alert("VELHA!!!");return
+    }
   }
-
 
   return (
     <div>
@@ -158,13 +225,8 @@ export default function Home() {
         <a onClick={copyText}><BsShare /></a>
       </div>
       <div className="players">
-        <h3>Jogador X</h3>
-        <h3>Jogador O</h3>
-      </div>
-      <div className="addPonitsPlayer">
-        <a id="dois" onClick={addPointsX}><IoIosAddCircleOutline/></a>
-        
-        <a id="dois" onClick={addPointsO}><IoIosAddCircleOutline/></a>
+        <h3>Jogador X<br></br><br></br>{x}</h3>
+        <h3>Jogador O<br></br><br></br>{o}</h3>
       </div>
     </div>
   )
